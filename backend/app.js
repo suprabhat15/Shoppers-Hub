505e5cbe8da0d849926e8635b91ec393140ec18a
Suprabhat Kumar <suprabhat2018@gmail.com>
2021-10-22 15:48:04 +0530

    user and password authentication

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const errorMiddleware = require('./middleware/error');
+const cookieParser = require('cookie-parser');
 
 app.use(express.json());
-
+app.use(cookieParser());
 
 // Import routes
 const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+
 app.use("/api/v1", product);
+app.use("/api/v1", user);
+
 app.use(errorMiddleware);
 
 module.exports = app;
