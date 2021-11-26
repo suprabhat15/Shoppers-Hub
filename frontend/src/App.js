c36be172efa035ddbc126410c5e097cde42d8301
suprabhat25 <suprabhat2018@gmail.com>
2021-11-26 16:24:17 +0530

    added actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
-import Header from './component/layout/Header.js';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { useEffect } from "react";
+import Header from './component/layout/Header';
+import Footer from './component/layout/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import WebFont from "webfontloader";
+import Home from './component/Home';
 
 function App() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+
   return (
     <Router>
       <Header />
+      <Routes>
+        <Route exact path="/" component={Home} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ frontend/src/component/layout/MetaData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+
+const MetaData = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MetaData
