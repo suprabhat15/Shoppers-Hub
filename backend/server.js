09fdc8b59d778d9d72cb49330993de8137e80705
suprabhat25 <suprabhat2018@gmail.com>
2021-12-11 02:27:45 +0530

    add cloudinary details

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 const connectDatabase = require('./config/database');
+const cloudinary = require('cloudinary');
 
 // Config
 dotenv.config({path:'backend/config/.config.env'});
 connectDatabase();
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY
+})
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is running at http://localhost:${process.env.PORT}`);
