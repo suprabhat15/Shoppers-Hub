ad6f73e5d80e870c14c25cdc65dbc401c3b7f5c5
suprabhat25 <suprabhat2018@gmail.com>
2021-12-11 03:31:11 +0530

    add UserOptions in Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,37 @@
 import './App.css';
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import Header from './component/layout/Header';
 import Footer from './component/layout/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import WebFont from "webfontloader";
+import WebFont from 'webfontloader';
 import Home from './component/Home';
 import ProductDetails from './component/Product/ProductDetails';
 import Products from './component/Product/Products';
 import Search from './component/Product/Search';
 import LoginSignUp from './component/User/LoginSignUp';
+import {loadUser} from './actions/userAction';
+import store from './store';
+import UserOptions from './component/layout/Header/UserOptions';
+import {useSelector} from 'react-redux';
 
 
 function App() {
+  const {isAuthenticated, user} = useSelector(state => state.user);
+
   useEffect(() => {
     WebFont.load({
       google: {
         families: ["Roboto", "Droid Sans", "Chilanka"],
       },
     });
+
+    store.dispatch(loadUser());
   }, []);
 
   return (
     <Router>
       <Header />
+        { isAuthenticated && <UserOptions user={user} /> }
         <Route exact path="/" component={Home} />
         <Route exact path="/product/:id" component={ProductDetails} />
         <Route exact path="/products" component={Products} />
---
+++ frontend/src/component/layout/Header/UserOptions.js
@@ -0,0 +1,68 @@
+import React, {Fragment} from 'react';
+import { SpeedDial, SpeedDialAction } from '@material-ui/lab';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import PersonIcon from '@material-ui/icons/Person';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import { useHistory } from 'react-router-dom';
+import { useAlert } from 'react-alert';
+
+const UserOptions = ({ user }) => {
+    const [open, setOpen] = useState(false);
+    const history = useHistory();
+
+    const options = [
+        {icon:<ListAltIcon/>, name:"Orders", func: orders},
+        {icon:<PersonIcon/>, name:"Profile", func: account},
+        {icon:<ExitToAppIcon/>, name:"Logout", func: logoutUser}
+    ]
+
+    if(user.role === 'admin'){
+        options.unshift({icon:<DashboardIcon/>, name:"Dashboard", func: dashboard});
+    }
+
+    function dashboard(){
+        history.push('/dashboard');
+    }
+
+    function orders(){
+        history.push('/orders');
+    }
+
+    function account(){
+        history.push('/account');
+    }
+
+    function logoutUser(){
+        dispatch(logout());
+        alert.success("Logout Successfully");
+    }
+
+
+    return (
+        <Fragment>
+            <SpeedDial
+                ariaLabel="SpeedDial tooltip example"
+                onClose={() => setOpen(false)}
+                onOpen={() => setOpen(true)}
+                open={open}
+                direction="down"
+                icon={
+                    <img
+                        className="speedDialIcon"
+                        src={user.avatar.url ? user.avatar.url : '/Profile.png'}
+                        alt="Profile"
+                    />
+                }
+            >
+            {
+                options.map(item => (
+                    <SpeedDialAction icon={item.icon} tooltipTitle={item.name} onClick={item.func} />
+                ))
+            }
+            </SpeedDial>
+        </Fragment>
+    )
+}
+
+export default UserOptions
